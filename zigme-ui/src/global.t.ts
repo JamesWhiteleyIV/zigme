3ccff407a8b2640e7dcf890a4cc6cd0ed609d7b8
James Whiteley IV <jwwiv@protonmail.com>
2024-02-23 18:07:57 -0800

    dockerfiles working; i think ui still needs SSL fix

--- zigme-ui/src/global.t.ts
+++ zigme-ui/src/global.t.ts
@@ -1,11 +1,11 @@
 type AlarmState = {
-    phone_alarms: boolean | null
-    phone_notifications: boolean | null
-    local_siren: boolean | null
-}
+	phone_alarms: boolean | null;
+	phone_notifications: boolean | null;
+	local_siren: boolean | null;
+};
 
 type AlarmEvent = {
-    title: string
-    message: string
-    timestamp: string
-}
+	title: string;
+	message: string;
+	timestamp: string;
+};
--- zigme-ui/src/routes/+page.server.ts
+++ zigme-ui/src/routes/+page.server.ts
@@ -2,11 +2,11 @@
 /** @type {import('./$types').PageServerLoad} */
 
 export async function load({ fetch }) {
-    const alarmStateResponse = await fetch(`${import.meta.env.VITE_ZIGME_API_URI}/alarm_state`);
-    const alarmEventsResponse = await fetch(`${import.meta.env.VITE_ZIGME_API_URI}/events`);
-    const alarmState: AlarmState = await alarmStateResponse.json();
-    const alarmEvents: AlarmEvent = await alarmEventsResponse.json();
-    const data = {alarmState, alarmEvents};
-    console.log(data);
-    return data;
+	const alarmStateResponse = await fetch(`${import.meta.env.VITE_ZIGME_API_URI}/alarm_state`);
+	const alarmEventsResponse = await fetch(`${import.meta.env.VITE_ZIGME_API_URI}/events`);
+	const alarmState: AlarmState = await alarmStateResponse.json();
+	const alarmEvents: AlarmEvent = await alarmEventsResponse.json();
+	const data = { alarmState, alarmEvents };
+	console.log(data);
+	return data;
 }
